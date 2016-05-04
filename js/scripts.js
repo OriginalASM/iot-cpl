1961f85f2eed3dcb0cef57f84c288b2ed8a24fb3
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2016-05-04 13:42:48 +0530

    fix fan
    Signed-off-by: Amitosh Swain Mahapatra <amitosh.swain@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,43 @@
-var host = 'ws://10.0.0.3:3001'
-var ws = new WebSocket(host);
+(new function() {
+    var host = 'ws://192.168.1.51:8000'
+    var ws = new WebSocket(host);
+    var fanSpeed = 0;
+    var fanSpeedUp = document.getElementById("fan-speed-up");
+    var needle = document.getElementById("animation-speedometer-needle");
+    fanSpeedUp.onclick = function(e) {
+        fanSpeed = Math.min(fanSpeed + 51, 255) //  255;
+        console.log(fanSpeed);
+        console.log("rotate(" + (-85 + 34 * (fanSpeed / 51)) + "deg)");
+        needle.style.transform = "rotate(" + (-85 + 34 * (fanSpeed / 51)) + "deg)";
+        ws.send('fan '+fanSpeed);
 
-var fanSpeed = 0;
+    }
+    var fanSpeedDown = document.getElementById("fan-speed-down");
+    fanSpeedDown.onclick = function(e) {
+        fanSpeed = Math.max(fanSpeed - 51, 0);
+        console.log(fanSpeed);
+        console.log("rotate(" + (-85 + 34 * (fanSpeed / 51)) + "deg)");
+        needle.style.transform = "rotate(" + (-85 + 34 * (fanSpeed / 51)) + "deg)";
+        ws.send('fan '+fanSpeed);
 
-var fanSpeedUp = document.getElementById("fan-speed-up");
-var needle = document.getElementById("animation-speedometer-needle");
+    }
+    ws.onmessage = function(event) {
+        console.log(event.data);
+    };
+    var light = document.getElementById("light-1-switch");
 
-fanSpeedUp.onclick = function (e) {
-	fanSpeed = fanSpeed + 51;//  255;
-	console.log(fanSpeed);
-	console.log("rotate("+(-85 + 34*(fanSpeed/51))+"deg)");
-	needle.style.transform = "rotate("+(-85 + 34*(fanSpeed/51))+"deg)"; 
-}
+    light.addEventListener("change", function() {
+        ws.send('light '+light.checked);
+    });
 
-var fanSpeedDown = document.getElementById("fan-speed-up");
+    var lightIntensity = document.getElementById("lightIntensity");
 
-fanSpeedDown.onclick = function (e) {
-	fanSpeed = Math.max(fanSpeed - 51,0);
-	console.log(fanSpeed);
-	console.log("rotate("+(-85 + 34*(fanSpeed/51))+"deg)");
-	needle.style.transform = "rotate("+(-85 + 34*(fanSpeed/51))+"deg)"; 
-}
+    lightIntensity,addEventListener("change", function() {
+    	ws.send('light '+lightIntensity.value);
+    });
 
-ws.onmessage = function(event) {
-    console.log(event.data);
-};
-var forward = document.getElementById("light-1-switch");
-forward.onclick = function() {
-    ws.send('f');
-};
-var left = document.getElementById("fan-1-switch");
-left.onclick = function() {
-    ws.send('l');
-};
+    var fan = document.getElementById("fan-1-switch");
+    fan.addEventListener("change", function() {
+        ws.send('fan '+fan.checked);
+    });
+})();
